7b0c8adc04be3095b60c806dfcdeb061c2aa3e6a
Sean Lang <slang800@gmail.com>
2016-04-14 00:11:14 -0400

    remove trailing colons from test names

--- test.js
+++ test.js
@@ -12,27 +12,27 @@ require('should');
 var stringify = require('./');
 
 describe('author', function () {
-  it('should stringify name:', function () {
+  it('should stringify name', function () {
     var author = {name: 'Jon Schlinkert'};
     stringify(author).should.equal('Jon Schlinkert');
   });
 
-  it('should stringify email:', function () {
+  it('should stringify email', function () {
     var author = {email: 'jon.schlinkert@sellside.com'};
     stringify(author).should.equal('<jon.schlinkert@sellside.com>');
   });
 
-  it('should stringify url:', function () {
+  it('should stringify url', function () {
     var author = {url: 'https://github.com/jonschlinkert'};
     stringify(author).should.equal('(https://github.com/jonschlinkert)');
   });
 
-  it('should strip trailing slashes from URL:', function () {
+  it('should strip trailing slashes from URL', function () {
     var author = {url: 'https://github.com/jonschlinkert/'};
     stringify(author).should.equal('(https://github.com/jonschlinkert)');
   });
 
-  it('should stringify name and url:', function () {
+  it('should stringify name and url', function () {
     var author = {
       name: 'Jon Schlinkert',
       url: 'https://github.com/jonschlinkert'
@@ -42,14 +42,14 @@ describe('author', function () {
     );
   });
 
-  it('should stringify name and email:', function () {
+  it('should stringify name and email', function () {
     var author = {name: 'Jon Schlinkert', email: 'jon.schlinkert@sellside.com'};
     stringify(author).should.equal(
       'Jon Schlinkert <jon.schlinkert@sellside.com>'
     );
   });
 
-  it('should stringify email and url:', function () {
+  it('should stringify email and url', function () {
     var author = {
       email: 'jon.schlinkert@sellside.com',
       url: 'https://github.com/jonschlinkert'
@@ -59,7 +59,7 @@ describe('author', function () {
     );
   });
 
-  it('should stringify name, email and url:', function () {
+  it('should stringify name, email and url', function () {
     var author = {
       name: 'Jon Schlinkert',
       email: 'jon@sellside.com',
