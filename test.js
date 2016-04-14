98b0bb04be89219a150788fe5eb3d538c9182abb
Sean Lang <slang800@gmail.com>
2016-04-14 00:08:58 -0400

    wrap lines longer than 80 chars

--- test.js
+++ test.js
@@ -33,22 +33,40 @@ describe('author', function () {
   });
 
   it('should stringify name and url:', function () {
-    var author = {name: 'Jon Schlinkert', url: 'https://github.com/jonschlinkert'};
-    stringify(author).should.equal('Jon Schlinkert (https://github.com/jonschlinkert)');
+    var author = {
+      name: 'Jon Schlinkert',
+      url: 'https://github.com/jonschlinkert'
+    };
+    stringify(author).should.equal(
+      'Jon Schlinkert (https://github.com/jonschlinkert)'
+    );
   });
 
   it('should stringify name and email:', function () {
     var author = {name: 'Jon Schlinkert', email: 'jon.schlinkert@sellside.com'};
-    stringify(author).should.equal('Jon Schlinkert <jon.schlinkert@sellside.com>');
+    stringify(author).should.equal(
+      'Jon Schlinkert <jon.schlinkert@sellside.com>'
+    );
   });
 
   it('should stringify email and url:', function () {
-    var author = {email: 'jon.schlinkert@sellside.com', url: 'https://github.com/jonschlinkert'};
-    stringify(author).should.equal('<jon.schlinkert@sellside.com> (https://github.com/jonschlinkert)');
+    var author = {
+      email: 'jon.schlinkert@sellside.com',
+      url: 'https://github.com/jonschlinkert'
+    };
+    stringify(author).should.equal(
+      '<jon.schlinkert@sellside.com> (https://github.com/jonschlinkert)'
+    );
   });
 
   it('should stringify name, email and url:', function () {
-    var author = {name: 'Jon Schlinkert', email: 'jon.schlinkert@sellside.com', url: 'https://github.com/jonschlinkert'};
-    stringify(author).should.equal('Jon Schlinkert <jon.schlinkert@sellside.com> (https://github.com/jonschlinkert)');
+    var author = {
+      name: 'Jon Schlinkert',
+      email: 'jon@sellside.com',
+      url: 'https://github.com/jonschlinkert'
+    };
+    stringify(author).should.equal(
+      'Jon Schlinkert <jon@sellside.com> (https://github.com/jonschlinkert)'
+    );
   });
 });
